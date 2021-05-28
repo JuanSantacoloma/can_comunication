a4c53873fa576758128f2584b0d971813e239985
JuanSantacolomaB <jssantacolomab@unal.edu.co>
2021-05-28 09:14:56 -0500

    showing recived messages

--- can_coms/scripts/can_coms.py
+++ can_coms/scripts/can_coms.py
@@ -4,10 +4,10 @@ import rospy
 from can_msgs.msg import Frame
 import std_msgs
 
-# import numpy as np 
+import numpy as np 
 # from rospy.numpy_msg import 
 
-class can_coms:
+class can_com:
 
     def __init__(self):
 
@@ -19,24 +19,25 @@ class can_coms:
 
         # Subscriptor
 
-        # rospy.Subscriber(self.nameTopicSub, can_msgs, self.can_clbk, queue_size=10)
+        rospy.Subscriber(self.nameTopicSub, Frame, self.can_clbk, queue_size=50)
         # Publicador
 
         self.pub = rospy.Publisher(self.nameTopicPub, Frame,queue_size=50)
 
-        while not rospy.is_shutdown():
+        # while not rospy.is_shutdown():
                 
-            new_can_msg = Frame()
-            new_can_msg.id = 1
-            new_can_msg.dlc = 8
-            new_can_msg.data = [0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xfd]
+        # new_can_msg = Frame()
+        # new_can_msg.id = 1
+        # new_can_msg.dlc = 8
+        # new_can_msg.data = [0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xfd]
             
-            self.pub.publish(new_can_msg)
+        #     self.pub.publish(new_can_msg)
 
-    # def can_clbk(self, can_msg):
-
-    #     new_can_msg = can_msgs
-    #     new_can_msg.
-    #     new_can_msg.data = [0xff,0xff,0xff,0xff,0xff,0xff,0xff,0xfd]
-        
-    #     self.pub.publish(new_can_msg)
+    def can_clbk(self, received_messages):
+        rec_msg_int = list(map(int, (received_messages.data).split(" \ ")))
+        print(rec_msg_int)
+        # payload = bytearray(received_messages.data)
+        # pay_array=[x for x in payload]
+        # print(hex(pay_array))
+        # print(type(received_messages.data))
+        # self.pub.publish(new_can_msg)
--- can_coms/scripts/to_can_coms_node.py
+++ can_coms/scripts/to_can_coms_node.py
@@ -2,13 +2,13 @@
 
 import rospy
 
-from can_coms import can_coms
+from can_coms import can_com
 
 if __name__ == '__main__':
 
     rospy.init_node('to_can_coms_node', anonymous=True)
     rospy.loginfo("Init node")
 
-    can_coms()
+    can_com()
     
     rospy.spin()
